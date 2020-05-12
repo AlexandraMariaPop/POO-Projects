3ea4ae101236f16e4780e0440948a6771ff1e310
Pop Alexandra Maria <alecsandramariaa@yahoo.com>
2020-05-12 15:55:23 +0300

    Add files via upload

---
+++ LR/LR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LR a = new LR("input.txt");
+            a.ReadData();
+            a.ProcessData();
+            a.ShowResults();
+        }
+    }
+}
