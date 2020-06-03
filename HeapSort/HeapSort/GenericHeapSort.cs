46dc83395e1aad0cce1ed34abc0be7cc0f1f9ff4
alecsandramariaa@yahoo.com <alecsandramariaa@yahoo.com>
2020-06-03 12:37:07 +0300

    Added generic type.

---
+++ HeapSort/HeapSort/GenericHeapSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    class GenericHeapSort<T> where T:IComparable<T>
+    {
+        public static int Parent(int i)
+        {
+            return i / 2;
+        }
+        public static int Left(int i)
+        {
+            return 2 * i + 1;
+        }
+        public static int Right(int i)
+        {
+            return 2 * i + 2;
+        }
+        public void Max_Heapify(T[] A, int i, int heapsize)
+        {
+            int l = Left(i);
+            int r = Right(i);
+            int largest = i;
+            if (l <= heapsize)
+            {  //&& (A[l] > A[i]))
+                int compare = A[l].CompareTo(A[i]);
+                if(compare>0)
+                largest = l;
+            }
+            if (r <= heapsize)
+            {//&& (A[r] > A[largest]))
+                int compare = A[r].CompareTo(A[largest]);
+                if(compare>0)
+                largest = r;
+            }
+            if (largest != i)
+            {
+                Swap(ref A[i], ref A[largest]);
+                Max_Heapify(A, largest, heapsize);
+            }
+
+        }
+        public void Build_Max_Heap(T[] A, int size)
+        {
+            int heapsize = A.Length - 1;
+            for (int i = heapsize / 2; i >= 0; i--)
+            {
+                Max_Heapify(A, i, size);
+            }
+        }
+        public void HeapSort(T[] A)
+        {
+            int heapsize = A.Length - 1;
+            Build_Max_Heap(A, heapsize);
+
+            for (int i = heapsize; i >= 1; i--)
+            {
+                Swap(ref A[0], ref A[i]);
+                heapsize--;
+                Max_Heapify(A, 0, heapsize);
+            }
+        }
+
+        private void Swap(ref T a, ref T b)
+        {
+            T aux = a;
+            a = b;
+            b = aux;
+        }
+    }
+}
--- HeapSort/HeapSort/Program.cs
+++ HeapSort/HeapSort/Program.cs
@@ -11,12 +11,21 @@ namespace HeapSort
         static void Main(string[] args)
         {
             int[] v = {55, 31, 5, 79, 1, 21, 22, 19, 66, 35 };
-            Heap hp = new Heap();
-            hp.HeapSort(v);
-            for (int i = 0; i <v.Length; i++)
+            Heap sort = new Heap();
+            sort.HeapSort(v);
+            for (int i = 0; i < v.Length; i++)
             {
                 Console.WriteLine(v[i]);
             }
+            Console.WriteLine("Generic:");
+            double[] k = { 23.5, 23.6, 29, 33,-1 };
+            GenericHeapSort<double> hp = new GenericHeapSort<double>();
+            hp.HeapSort(k);
+            for (int i = 0; i <k.Length; i++)
+            {
+                Console.WriteLine(k[i]);
+            }
+            
             Console.ReadKey();
         }
     }
