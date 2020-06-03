6c5437a20c9f5f72c64b94e48ca955e9a2dcbf9f
Pop Alexandra Maria <alecsandramariaa@yahoo.com>
2020-06-03 13:39:10 +0300

    Added new project.

---
+++ ClasaStiva/ClasaStiva/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasaStiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Stiva<int> s1 = new Stiva<int>(20);
+           s1.Push(5);
+           s1.Push(4);
+           s1.Push(43);
+           s1.Push(14);
+           //s1.Clear();
+           Console.WriteLine( s1.Pop());
+           Console.WriteLine(s1);
+            Console.WriteLine();
+
+            Stiva<string> s2 = new Stiva<string>(20);
+            s2.Push("ab");
+            s2.Push("c");
+            s2.Push("DE");
+            s2.Push("A");
+            //s1.Clear();
+            Console.WriteLine(s2.Pop());
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClasaStiva/ClasaStiva/Stiva.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ClasaStiva
+{
+    public class Stiva<T>
+    {
+        private List<T> lista;
+        private int size;
+        public Stiva(int size)
+        {
+            lista = new List<T>();
+            this.size = size;
+        }
+        //Push() – pentru introducerea de elemente în stivă,
+        public void Push(T toadd)
+        {
+            if (lista.Count < size)
+                lista.Add(toadd);
+            else
+                throw new Exception("STACK IS FULL");
+        }
+        //Pop() – pentru extragerea de elemente din stivă,
+        public T Pop()
+        {
+            if (lista.Count != 0)
+            {
+                T toreturn = lista[lista.Count-1];
+                lista.Remove(toreturn);
+                return toreturn;
+               
+            }
+            else
+                throw new Exception("STACK IS EMPTY");
+        }
+        //Clear() – pentru ștergerea tuturor elementelor din stivă.
+        public void Clear()
+        {
+            if (lista.Count != 0)
+                lista.Clear();
+            else
+                throw new Exception("STACK IS ALREADY EMPTY");
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in lista)
+            {
+                sb.Append(item.ToString() + " ");
+            }
+            return sb.ToString();
+        }
+    }
+}
