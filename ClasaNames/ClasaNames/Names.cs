0c349d62fe11c08713de3cfce3813c7b62fe696b
Pop Alexandra Maria <alecsandramariaa@yahoo.com>
2020-06-03 14:54:35 +0300

    Added new project.

---
+++ ClasaNames/ClasaNames/Names.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClasaNames
+{
+    internal class Names
+    {
+        private int dimensiune;
+        private List<string> nume;
+        public Names(int dimensiune)
+        {
+            this.dimensiune = dimensiune;
+            nume = new List<string>(dimensiune);
+            for (int i = 0; i < dimensiune; i++)
+            {
+                nume.Add("Default");
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if ((index >= 0) && (index < dimensiune))
+                {
+                    string toreturn = nume.ElementAt(index);
+                    return toreturn;
+                }
+                else throw new IndexOutOfRangeException("INDEX WAS OUT OF RANGE");
+            }
+            set
+            {
+                if ((index >= 0) && (index < dimensiune))
+                {
+                    nume.Insert(index, value);
+                    nume.RemoveAt(dimensiune );
+                }
+                else
+                    throw new IndexOutOfRangeException("INDEX WAS OUT OF RANGE");
+            }
+        }
+        public string this[string index]
+        {
+            get
+            {
+                int idx ;
+                bool valid = true;
+                try
+                {
+                     idx = int.Parse(index);
+                }
+                catch(FormatException)
+                {
+                    valid = false;
+                    throw new Exception("NOT A VALID NUMBER");
+                }
+                if (valid)
+                {
+                    idx = int.Parse(index);
+                    if ((idx >= 0) && (idx < dimensiune))
+                    {
+                        string toreturn = nume.ElementAt(idx);
+                        return toreturn;
+                    }
+                    else throw new IndexOutOfRangeException("INDEX WAS OUT OF RANGE");
+                }
+                else
+                {
+                    throw new Exception("NOT A VALID NUMBER");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in nume)
+            {
+                sb.Append(item + " ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ClasaNames/ClasaNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasaNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names n = new Names(3);
+            n[0] = "Nume";
+            n[1] = "prenume 1";
+            n[2] = "prenume 2";
+           Console.WriteLine(n);
+            //n[5] = "X";
+            // Console.WriteLine(n["1"]);
+            Console.WriteLine(n["2"]);
+            Console.ReadKey();
+        }
+    }
+}
